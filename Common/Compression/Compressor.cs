02833f89031aacbf0d8c62139ce379b6abb13d68
jensfu1337 <jensfu1337@gmail.com>
2016-03-12 16:11:23 +0100

    Added simple compression with GZipStream

---
+++ Common/Compression/Compressor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Common.Compression
+{
+    public static class Compressor
+    {
+        public static byte[] CompressByte(byte[] data)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                using (GZipStream gzip = new GZipStream(memory, CompressionMode.Compress, true))
+                {
+                    gzip.Write(data, 0, data.Length);
+                }
+                return memory.ToArray();
+            }
+        }
+    }
+}
