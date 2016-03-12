0c54c42fc0d90c39811af84a2f137420dbcf7448
jensfu1337 <jensfu1337@gmail.com>
2016-03-12 17:51:40 +0100

    Fixed decompression method

--- Common/Compression/Compressor.cs
+++ Common/Compression/Compressor.cs
@@ -6,15 +6,41 @@ namespace Common.Compression
 {
     public static class Compressor
     {
-        public static byte[] CompressByte(byte[] data)
+        public static byte[] CompressBytes(byte[] data)
         {
             using (MemoryStream memory = new MemoryStream())
             {
-                using (GZipStream gzip = new GZipStream(memory, CompressionMode.Compress, true))
+                using (GZipStream gzip = new GZipStream(memory, CompressionMode.Compress))
                 {
                     gzip.Write(data, 0, data.Length);
                 }
+
                 return memory.ToArray();
+            }
+        }
+
+        public static byte[] DecompressBytes(byte[] gzip)
+        {
+            // Create a GZIP stream with decompression mode.
+            // ... Then create a buffer and write into while reading from the GZIP stream.
+            using (GZipStream stream = new GZipStream(new MemoryStream(gzip), CompressionMode.Decompress))
+            {
+                const int size = 4096;
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = stream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    return memory.ToArray();
+                }
             }
         }
     }
