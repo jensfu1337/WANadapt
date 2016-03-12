2321cd8156a4ec3c04acfc5016b14137045b4025
jensfu1337 <jensfu1337@gmail.com>
2016-03-12 15:59:07 +0100

    Added command class

---
+++ Common/Commands/CommandBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Commands
+{
+    public enum CommandType
+    {
+        Message,
+        Popup,
+        Null
+    }
+
+    [Serializable]
+    public abstract class CommandBase
+    {
+        public CommandType Type { get; protected set; } = CommandType.Null;
+        protected byte[] Data { get; set; }
+
+        public CommandBase()
+        {
+        }
+    }
+}
---
+++ Common/Commands/Commands.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Commands
+{
+    public sealed class Message : CommandBase
+    {
+        public Message()
+        {
+            this.Type = CommandType.Message;
+        }
+    }
+
+    public sealed class Popup : CommandBase
+    {
+        public Popup()
+        {
+            this.Type = CommandType.Popup;
+        }
+    }
+}
